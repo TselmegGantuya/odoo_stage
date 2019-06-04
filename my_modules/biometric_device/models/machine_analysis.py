bae4d9d0bcd0bd318aad183b81274b05a20f915a
LAPTOP-J6F0OR4J\TSelmeg <99040437@mydavinci.nl>
2019-06-04 10:08:46 +0800

    Finalizing biometric device module
     -Counting lunch breaks
    - Possiblity to import attendance
    - Now you can download the data to the log and turn them into attendance after
    - some minor changes

--- my_modules/biometric_device/models/machine_analysis.py
+++ my_modules/biometric_device/models/machine_analysis.py
@@ -24,67 +24,13 @@ from odoo import models, fields, api, _
 
 class ZkMachine(models.Model):
     _name = 'zk.machine.attendance'
-    _inherit = 'hr.attendance'
     _order = 'punching_time asc'
 
-    @api.constrains('check_in', 'check_out', 'employee_id')
-    def _check_validity(self):
-        """overriding the __check_validity function for employee attendance."""
-        pass
-
+    employee_id = fields.Many2one('hr.employee', string="Employee", ondelete='cascade', index=True)
     device_user_name = fields.Char(string='Biometric Device User Name')
-    punch_type = fields.Selection([('0', 'Check In'),
-                                   ('1', 'Check Out')],
-                                  string='Punching Type')
-    day_period = fields.Selection([('morning', 'Morning'),
-                                   ('afternoon', 'Afternoon')], string='Day period')
 
     attendance_type = fields.Selection([('1', 'Finger'),
                                         ('15', 'Face')], string='Category')
     punching_time = fields.Datetime(string='Punching Time')
-    punching_date = fields.Datetime(string='Punching Date')
-    address_id = fields.Many2one('res.partner', string='Working Address')
-
-
-class ReportZkDevice(models.Model):
-    _name = 'zk.report.daily.attendance'
-    _auto = False
-    _order = 'punching_day desc'
-
-    name = fields.Many2one('hr.employee', string='Employee')
-    punching_day = fields.Datetime(string='Date')
-    address_id = fields.Many2one('res.partner', string='Working Address')
-    attendance_type = fields.Selection([('1', 'Finger'),
-                                        ('15', 'Face')],
-                                       string='Category')
-    punch_type = fields.Selection([('0', 'Check In'),
-                                   ('1', 'Check Out')], string='Punching Type')
-    punching_date = fields.Datetime(string='Punching Date')
-    punching_time = fields.Datetime(string='Punching Time')
-
-    def init(self):
-        tools.drop_view_if_exists(self._cr, 'zk_report_daily_attendance')
-        query = """
-            create or replace view zk_report_daily_attendance as (
-                select
-                    min(z.id) as id,
-                    z.employee_id as name,
-                    z.write_date as punching_day,
-                    z.address_id as address_id,
-                    z.attendance_type as attendance_type,
-                    z.punching_time as punching_time,
-                    z.punch_type as punch_type
-                from zk_machine_attendance z
-                    join hr_employee e on (z.employee_id=e.id)
-                GROUP BY
-                    z.employee_id,
-                    z.write_date,
-                    z.address_id,
-                    z.attendance_type,
-                    z.punch_type,
-                    z.punching_time
-            )
-        """
-        self._cr.execute(query)
 
 
--- my_modules/biometric_device/models/models.py
+++ my_modules/biometric_device/models/models.py
@@ -33,7 +33,7 @@ class BiometricDevice(models.Model):
 
     @api.multi
     def time_to_float(self, time):
-         return int(time.strftime("%H")) + int(time.strftime("%M")) / 60.0
+        return int(time.strftime("%H")) + int(time.strftime("%M")) / 60.0
 
     @api.multi
     def clean_attendance(self):
@@ -48,9 +48,7 @@ class BiometricDevice(models.Model):
     @api.multi
     def download_attendance(self):
         zk_attendance = self.env['zk.machine.attendance']
-        hr_attendance = self.env['hr.attendance']
         hr_employee_obj = self.env['hr.employee']
-        res_company = self.env.user.company_id
         conn = self.device_connect()
         device_users = conn.get_users()
         time_def = self.device_timezone
@@ -62,21 +60,9 @@ class BiometricDevice(models.Model):
             if attendances:
                 for attendance in attendances:
                     atten_time = attendance.timestamp
-                    atten_hours = self.time_to_float(atten_time)
-                    if atten_hours < res_company.deny_check_in_time_before or atten_hours > res_company.deny_check_out_time_after:
-                        continue
-                    atten_dayofweek = atten_time.weekday()
-                    if atten_dayofweek == 5 or atten_dayofweek == 6:
-                        continue
-                    atten_date = atten_time.strftime("%Y-%m-%d")
                     atten_time = datetime.strptime(
                         atten_time.strftime('%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S')
-                    if self.device_timezone < 0:
-                        atten_time = atten_time - dt.timedelta(hours=time_def)
-                    elif self.device_timezone > 0:
-                        atten_time = atten_time - dt.timedelta(hours=time_def)
-                    atten_time = datetime.strptime(
-                        atten_time.strftime('%Y-%m-%d %H:%M:%S'), "%Y-%m-%d %H:%M:%S")
+                    atten_time = atten_time - dt.timedelta(hours=time_def)
                     atten_time = fields.Datetime.to_string(atten_time)
                     hr_employee = None
                     for device_user in device_users:
@@ -89,65 +75,87 @@ class BiometricDevice(models.Model):
                         if duplicate_atten_ids:
                             continue
                         else:
-                            punch_type = None
-                            def_hour = None
-                            day_period = None
-                            working_hours = hr_employee[0].resource_calendar_id.attendance_ids
-                            for working_hour in working_hours:
-                                if int(working_hour.dayofweek) == atten_dayofweek:
-                                    if working_hour.day_period == "morning" and working_hour.hour_to > atten_hours:
-                                        def_hour = self.float_to_time(working_hour.hour_to - time_def)
-                                        day_period = "morning"
-                                        break
-                                    elif working_hour.day_period == "afternoon" and working_hour.hour_from < atten_hours:
-                                        def_hour = self.float_to_time(working_hour.hour_from - time_def)
-                                        day_period = "afternoon"
-                                        break
-                            if day_period is None:
-                                continue
-                            prev_atten_ids = zk_attendance.search(
-                                [('device_user_name', '=', hr_employee[0].device_user_name), ('punching_date', '=', atten_date),
-                                 ('day_period', '=', day_period)])
-                            if prev_atten_ids:
-                                att_var = hr_attendance.search([('employee_id', '=', hr_employee[0].id),
-                                                                ('check_out', '=', prev_atten_ids[0].punching_time)])
-                                if att_var:
-                                    if att_var.check_in < datetime.strptime(atten_time, "%Y-%m-%d %H:%M:%S"):
-                                        att_var[0].write({'check_out': atten_time})
-                                        punch_type = '1'
-                            else:
-                                if day_period == 'morning':
-                                    if datetime.strptime(atten_time, "%Y-%m-%d %H:%M:%S") < datetime.strptime(
-                                            atten_date + ' ' + def_hour + ':00', "%Y-%m-%d %H:%M:%S"):
-                                        hr_attendance.create({'employee_id': hr_employee[0].id,
-                                                              'check_in': atten_time,
-                                                              'day_period': day_period,
-                                                              'check_out': atten_date + ' ' + def_hour + ':00'})
-                                    else:
-                                        raise UserError([atten_time, atten_hours])
-
-                                elif day_period == 'afternoon':
-                                    if datetime.strptime(atten_time, "%Y-%m-%d %H:%M:%S") > datetime.strptime(
-                                            atten_date + ' ' + def_hour + ':00', "%Y-%m-%d %H:%M:%S"):
-                                        hr_attendance.create({'employee_id': hr_employee[0].id,
-                                                              'check_in': atten_date + ' ' + def_hour + ':00',
-                                                              'day_period': day_period,
-                                                              'check_out':  atten_time})
-                                    else:
-                                        raise UserError([atten_time, atten_hours, atten_date + ' ' + def_hour + ':00'])
-                                else:
-                                    raise UserError(day_period)
-                                punch_type = '0'
                             zk_attendance.create({'employee_id': hr_employee[0].id,
                                                   'device_user_name': hr_employee[0].device_user_name,
                                                   'attendance_type': str(attendance.status),
-                                                  'punch_type': punch_type,
-                                                  'punching_date': atten_date,
-                                                  'punching_time': atten_time,
-                                                  'day_period': day_period})
+                                                  'punching_time': atten_time})
                     else:
                         pass
             conn.enable_device()
+
+    def make_attendance(self):
+        zk_attendance = self.env['zk.machine.attendance']
+        hr_attendance = self.env['hr.attendance']
+        hr_employee_obj = self.env['hr.employee']
+        res_company = self.env.user.company_id
+        time_def = self.device_timezone
+
+        for attendance in zk_attendance.search([]):
+            if attendance.employee_id:
+                pass
+            else:
+                hr_employee = hr_employee_obj.search([('device_user_name', '=', attendance.device_user_name)])
+                if hr_employee:
+                    attendance.write({'employee_id': hr_employee.id})
+                else:
+                    continue
+
+            atten_time = attendance.punching_time
+            atten_hours = self.time_to_float(atten_time)
+            if atten_hours + time_def < res_company.deny_check_in_time_before or atten_hours + time_def > res_company.deny_check_out_time_after:
+                continue
+            atten_dayofweek = atten_time.weekday()
+            if atten_dayofweek == 5 or atten_dayofweek == 6:
+                continue
+            atten_date = atten_time.strftime('%Y-%m-%d')
+            def_hour = None
+            day_period = None
+            working_hours = attendance.employee_id.resource_calendar_id.attendance_ids
+            for working_hour in working_hours:
+                if int(working_hour.dayofweek) == atten_dayofweek:
+                    if working_hour.day_period == "morning" and working_hour.hour_to > atten_hours + time_def:
+                        def_hour = self.float_to_time(working_hour.hour_to - time_def)
+                        day_period = "morning"
+                        break
+                    elif working_hour.day_period == "afternoon" and working_hour.hour_from - time_def < atten_hours:
+                        def_hour = self.float_to_time(working_hour.hour_from - time_def)
+                        day_period = "afternoon"
+                        break
+            prev_atten_ids = None
+
+            if day_period == 'morning':
+                prev_atten_ids = hr_attendance.search(
+                    [('employee_id', '=', attendance.employee_id.id),
+                     ('check_out', '=', atten_date + ' ' + def_hour + ':00'),
+                     ('day_period', '=', day_period)])
+            elif day_period == 'afternoon':
+                prev_atten_ids = hr_attendance.search(
+                    [('employee_id', '=', attendance.employee_id.id),
+                     ('check_in', '=', atten_date + ' ' + def_hour + ':00'),
+                     ('day_period', '=', day_period)])
+            else:
+                continue
+            if prev_atten_ids:
+                if day_period == 'morning':
+                    if prev_atten_ids.check_in > atten_time:
+                        prev_atten_ids.write({'check_in': atten_time})
+                elif day_period == 'afternoon':
+                    if prev_atten_ids.check_out < atten_time:
+                        prev_atten_ids.write({'check_out': atten_time})
+            else:
+                if day_period == 'morning':
+                    hr_attendance.create({'employee_id': attendance.employee_id.id,
+                                          'check_in': atten_time,
+                                          'day_period': day_period,
+                                          'state': 0,
+                                          'check_out': atten_date + ' ' + def_hour + ':00'})
+                elif day_period == 'afternoon':
+                    hr_attendance.create({'employee_id': attendance.employee_id.id,
+                                          'check_in': atten_date + ' ' + def_hour + ':00',
+                                          'day_period': day_period,
+                                          'state': 0,
+                                          'check_out': atten_time})
+
 
 
 class HrAttendance(models.Model):
@@ -156,6 +164,13 @@ class HrAttendance(models.Model):
     device_user_name = fields.Char(string='Biometric Device User Name')
     day_period = fields.Selection([('morning', 'Morning'),
                                    ('afternoon', 'Afternoon')], string='Day period')
+    state = fields.Selection([(0, 'Auto Input'), (1, 'Manual input'), (2, 'Done'), (3, 'Cancelled')],
+                             string='State')
+
+    @api.onchange('check_in', 'check_out')
+    def _onchange_attendance(self):
+        if self.state == 0:
+            self.state = 1
 
 
 class HrEmployee(models.Model):
